f0daf094598857dc4ab642d9fcfc1e031df3d9df
Nischal Bastola <nischalbastola00139@gmail.com>
2026-06-23 18:56:59 +0545

    tested on remaining dataset, manually chose sick threshold.

--- model.py
+++ model.py
@@ -35,7 +35,7 @@ x_test_scaled[scaling_columns] = (x_test[scaling_columns]-train_means) / train_s
 
 x = x_train_scaled.values
 y = y_train.values
-iterations = 100000
+iterations = 10000
 m = len(x_train_scaled)
 learning_rate = 1
 w = np.zeros(15)
@@ -45,6 +45,7 @@ for i in range(iterations):
  g = np.dot(x, w) + b
  predictions = 1 / (1 + np.exp(-g))
  cost = -1/m * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
+
  error = predictions - y
  w = w - (learning_rate/m) * np.dot(x.T, error)
  b = b - (learning_rate/m) * np.sum(error)
@@ -54,5 +55,53 @@ for i in range(iterations):
 
 print(f'Final Weights: {w}, final bias {b}')
 
+# Seeing result on test data
+
+y_2 = y_test.values
+x_2 = x_test_scaled.values
+
+predictions_test = 1 / (1 + np.exp(-(np.dot(x_2, w) + b)))
+predictions_tf = predictions_test >= 0.152
+
+correct_hits = np.array((predictions_tf == y_2)).astype(int)
+num_acc = correct_hits.sum()
+accuracy = (num_acc/len(y_2)) * 100
+print(f"Accuracy of Model : {accuracy:.2f}% ")
+print(num_acc)
+
+# Seeing the recall
+c = 0
+for i in range(len(y_2)):
+ if (y_2[i] == 1) & (np.array(predictions_tf[i]).astype(int) == 1):
+  c=c+1
+
+recall = c/np.sum(y_2)
+print(f"Recall: {recall * 100:.2f}%")
 
 
+"""
+
+Accuracy of Model : 67.57% 
+Recall: 67.67%
+
+Final Weights: 
+
+male: 0.4142
+age: 0.5631
+education: -0.0088
+currentSmoker: -0.1454
+cigsPerDay: 0.3011
+BPMeds: 0.2740
+prevalentStroke: 0.7070
+prevalentHyp: 0.3653
+diabetes: -0.0959
+totChol: 0.0701
+sysBP: 0.2041
+diaBP: -0.0210
+BMI: -0.0078
+heartRate: 0.0033
+glucose: 0.2011
+
+Final Bias: -2.19957
+
+"""
